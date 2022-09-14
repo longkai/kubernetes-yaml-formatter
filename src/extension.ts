3c84cfa3856af1f15e1a6cecdc6c3362811137df
longkai <im.longkai@gmail.com>
2022-09-14 23:17:52 +0800

    feat: add config option to the vscode settings

--- src/extension.ts
+++ src/extension.ts
@@ -21,6 +21,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('kubernetes-yaml-formatter.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
+		console.log(`${vscode.workspace.getConfiguration().get('kubernetes-yaml-formatter.compactSequenceIndent')}`);
 		vscode.window.showInformationMessage(`Hello World from Kubernetes YAML Formatter!`);
 	});
 
