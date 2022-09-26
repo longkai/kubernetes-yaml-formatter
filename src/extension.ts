468586a58fcc87a3c9370ab0212af159c090ce1b
longkai <im.longkai@gmail.com>
2022-09-26 15:21:08 +0000

    chore: clean scaffold hello world

--- src/extension.ts
+++ src/extension.ts
@@ -27,17 +27,6 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('kubernetes-yaml-formatter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage(`Hello World from Kubernetes YAML Formatter!`);
-	});
-
-	context.subscriptions.push(disposable);
-
 	// 👍 formatter implemented using API
 	vscode.languages.registerDocumentFormattingEditProvider('yaml', {
 		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
