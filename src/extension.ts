4be0433ba421a651c840003f113ec69d0bb614a1
Kilian Tyler <github@ktyler.me>
2024-02-26 19:42:58 -0500

    chore: mostly linting

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log(`Congratulations, your extension "kubernetes-yaml-formatter-x" is now active!`);
-  
+
 	writeConfigFile(context);
 	vscode.workspace.onDidChangeConfiguration(ev => {
       if (ev.affectsConfiguration('kubernetes-yaml-formatter-x')) {
